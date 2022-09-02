069ca7f60ef5c11cde22d09490f80a1d2502f721
keita1115 <keita.hashimoto1115@gmail.com>
2022-09-02 06:20:52 +0000

    問題27完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
@@ -45,4 +46,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end
+
+   def ensure_correct_user
+     @book = Book.find(params[:id])
+      unless @book.user == current_user
+        redirect_to books_path
+      end
+    end
+   end

c13047f52616d6095922b1bccb387ba13f5f5548
keita1115 <keita.hashimoto1115@gmail.com>
2022-09-05 10:41:36 +0000

    search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+ 
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
   resource :favorites, only: [:create, :destroy]
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
   get 'followings' => 'relationships#followings', as: 'followings'
   get 'followers' => 'relationships#followers', as: 'followers'
 end
+ get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 

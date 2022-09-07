329122c98955293a702bbed2593aecfb1ca7b3fd
keita1115 <keita.hashimoto1115@gmail.com>
2022-09-07 04:19:58 +0000

    非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
+    #render :comment
   end
 
   def destroy
     book = BookComment.find(params[:id])
     book.destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    #render :comment
   end
 
   private
